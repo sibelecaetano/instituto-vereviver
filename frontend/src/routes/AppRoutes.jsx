5ba028c8c5b16b1e51e3a90ac9ef902820183cbd
EmanoelLimaaa <limaemanoe91@gmail.com>
2026-02-27 19:23:41 -0300

    Correção de rota

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import PainelAdm from "../pages/PainelAdm/PainelAdm";
+import Login from "../pages/Login/Login";
 import Layout from "../components/Layout/Layout";
 import SobreOInstituto from "../components/SobreOInstituto/SobreOInstituto";
 
@@ -8,9 +9,10 @@ export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<PainelAdm />} />
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="admin" element={<PainelAdm />} />
         </Route>
       </Routes>
     </BrowserRouter>
